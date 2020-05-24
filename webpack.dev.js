c4695f0c1cd63bdb2ba4cee2eabde02914489f61
Anshuman Gupta <anshuman844@gmail.com>
2020-05-24 13:13:00 +0530

    feat: Add webpack development configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,38 +4,71 @@ const common = require('./webpack.common');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = merge(common, {
-  mode: "development",
+  mode: 'development',
+  devtool: 'cheap-source-map',
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "build")
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    publicPath: '/'
   },
   plugins: [
     new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.resolve(__dirname, "src/index.html"),
-      chunks: [ "main" ]
-    }),
-    new HtmlWebpackPlugin({
-      filename: "readme/index.html",
-      template: path.resolve(__dirname, "src/readme/index.html"),
-      chunks: [ "readme" ] // more chunks can be added. Chunk name is same as keys in entrypoint.
+      filename: 'index.html',
+      template: path.resolve(__dirname, 'public/index.html'),
+      chunks: [ 'main' ]
     })
-    // new HtmlWebpackPlugin for each html file.
   ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [ "style-loader", "css-loader" ]
+        use: [
+          'style-loader', 
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]--[hash:base64:5]'
+              }
+            }
+          }
+        ]
       },
       {
         test: /\.s[ac]ss$/,
-        use: [ "style-loader", "css-loader", "sass-loader" ]
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                mode: 'local',
+                localIdentName: '[name]__[local]--[hash:base64:5]'
+              }
+            }
+          },
+          'sass-loader'
+        ]
       },
       {
-        test: /\.(svg|png|jpg|gif|jpeg)$/,
-        use: ["file-loader"]
+        test: /\.(ico|svg|png|gif|jpe?g)$/,
+        use: [ 'file-loader' ]
       }
     ]
+  },
+  stats: 'errors-warnings',
+  devServer: {
+    contentBase: path.join(__dirname, 'build'),
+    compress: true,
+    clientLogLevel: 'trace',
+    watchContentBase: true,
+    hot: true,
+    publicPath: '/',
+    port: 9000,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
   }
 });
